fd87f9aca26136008b197c1203a084e7daf6ff72
victorhpf97 <50004902+victorhpf97@users.noreply.github.com>
2019-05-21 01:30:53 -0300

    Add files via upload

---
+++ manipular arquivos.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main () {
+	
+	FILE *ponteiro;
+	ponteiro=fopen("C:\\Users\\ERICA\\Desktop\\teste.txt","w");
+	fprintf(ponteiro,"o rato roeu a roupa do rei de roma.");
+	fclose(ponteiro);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
