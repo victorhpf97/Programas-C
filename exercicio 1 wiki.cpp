af757a5b93c276a256b4aa01c7ccedf700742e8e
victorhpf97 <50004902+victorhpf97@users.noreply.github.com>
2019-05-25 20:37:54 -0300

    Add files via upload

---
+++ exercicio 1 wiki.cpp
@@ -0,0 +1,42 @@
+//Exercício 1
+//Escreva uma função que peça dois números inteiros ao usuário e exibe o valor soma seguido pelo maior deles
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main () {
+	int x;
+	int y;
+	
+	
+	
+	printf("Digite o valor de x: ");
+	scanf("%i",&x);
+	printf("Digite o valor de y: ");
+	scanf("%i",&y);
+	if(x>y)(printf("O maior é %d e a soma dos numeros e %d ",x,x+y));
+	if(x<y)(printf("O maior é %d e a soma dos numeros e %d ",y,x+y));
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
