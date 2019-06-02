eee0fa09feaca9c0ec7dcb788a56a3697444c21e
victorhpf97 <50004902+victorhpf97@users.noreply.github.com>
2019-06-01 23:38:51 -0300

    Add files via upload

---
+++ função em c.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+ 
+
+int main (){
+	
+	void funcao(void);
+	
+	funcao();
+	funcao();
+	funcao();
+	
+	return 0;
+}
+	funcao(void){
+		
+		
+		printf("Esta e minha primeira funcao\n");
+	}
+	
